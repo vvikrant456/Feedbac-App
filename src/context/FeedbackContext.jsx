f49eb31771826e5c54f49c681555901a3d009a04
VikasPc <Your Email Address Here>
2022-04-30 21:48:50 +0530

    json

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,31 +1,30 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is feedback item1',
-      rating: 7,
-    },
-    {
-      id: 2,
-      text: 'This is feedback item2',
-      rating: 10,
-    },
-    {
-      id: 3,
-      text: 'This is feedback item3',
-      rating: 8,
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(()=>{
+    // console.log(123)
+    fetchFeedback()
+  },[]);
+
+  //Fetch Feedback
+  const fetchFeedback = async()=>{
+    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`);
+    const data = await response.json();
+    // console.log(data);
+    setFeedback(data);
+    setIsLoading(false);
+  }
 
   //add feedback
   const addFeedback = (newFeedback) => {
@@ -64,6 +63,7 @@ export const FeedbackProvider = ({ children }) => {
         addFeedback,
         editFeedback,
         feedbackEdit,
+        isLoading,
         updateFeedback,
       }}
     >
